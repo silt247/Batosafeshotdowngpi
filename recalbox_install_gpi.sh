21bb41c08c4ef5623805fdd5363c53e10f86abac
silt247 <52610091+silt247@users.noreply.github.com>
2019-07-06 13:22:27 -0700

    Update recalbox_install_gpi.sh

--- recalbox_install_gpi.sh
+++ recalbox_install_gpi.sh
@@ -3,7 +3,6 @@
 sleep 2s
 filewebsite="https://raw.githubusercontent.com/RetroFlag/retroflag-picase/master"
 
-mount -o remount, rw /boot
 mount -o remount, rw /
 
 #Step 3) Download Python script-----------------------------
