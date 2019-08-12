7edcc2e9535ebec385d861cf5b76f973834cfef2
silt247 <52610091+silt247@users.noreply.github.com>
2019-08-11 21:47:11 -0700

    Update Batocera_install_gpi.sh

--- Batocera_install_gpi.sh
+++ Batocera_install_gpi.sh
@@ -43,7 +43,7 @@ fi
 #Step 5) Apply changes----------------------------
 echo "RetroFlag Pi Case Switch installation done. Saving changes."
 
-/batocera/scripts/save-overlay.sh
+/batocera-save-overlay.sh
 
 echo "RetroFlag Pi Case Switch installation done. Will now reboot after 4 seconds."
 sleep 4
